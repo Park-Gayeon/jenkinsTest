6f296dfe3311be5a282c1d4701248c9eebe9b1be
parkGaYeon <gay0830@naver.com>
2025-03-04 19:36:37 +0900

    commit test중중

--- src/test/java/com/example/jenkinstest/JenkinsTestApplicationTests.java
+++ src/test/java/com/example/jenkinstest/JenkinsTestApplicationTests.java
@@ -10,6 +10,7 @@ class JenkinsTestApplicationTests {
     void contextLoads() {
         System.out.println("Hello, Jenkins!");
         System.err.println("변경사항을 추가한다");
+        System.out.println("새로운 브랜치 테스트중입니다");
     }
 
 }
