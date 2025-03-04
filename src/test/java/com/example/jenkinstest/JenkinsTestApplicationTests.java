2b08c9e939a08badf8b15b10afcb5537bf9ec153
parkGaYeon <gay0830@naver.com>
2025-03-04 20:03:07 +0900

    충돌 test

--- src/test/java/com/example/jenkinstest/JenkinsTestApplicationTests.java
+++ src/test/java/com/example/jenkinstest/JenkinsTestApplicationTests.java
@@ -9,7 +9,6 @@ class JenkinsTestApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("Hello, Jenkins!");
-        System.err.println("변경사항을 추가한다");
         System.out.println("새로운 브랜치 테스트중입니다");
     }
 
